9334e871868419133828c30a69e0d83cb7131a1a
Michael Hadley <mikethadley@gmail.com>
2020-04-22 09:43:28 -0700

    Simpliy nix config

--- shell.nix
+++ shell.nix
@@ -1,10 +1,17 @@
 let
-  pkgs = import ./nix/packages.nix;
+  pkgs = import (fetchTarball {
+    url =  https://github.com/NixOS/nixpkgs/archive/10100a97c8964e82b30f180fda41ade8e6f69e41.tar.gz;
+    # Generate with nix-prefetch-url --unpack --type sha256 [url]
+    sha256 = "011f36kr3c1ria7rag7px26bh73d1b0xpqadd149bysf4hg17rln";
+  }) {};
 in
 pkgs.mkShell {
-  inputsFrom = [ (import ./default.nix) ];
-
   buildInputs = with pkgs; [
     entr
+    gnumake
+    nodejs
+    elmPackages.elm
+    elmPackages.elm-format
+    elmPackages.elm-test
   ];
 }
