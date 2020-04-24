1e4e7778d7b59d5a168dc9a8451575c09ea460e4
Michael Hadley <mikethadley@gmail.com>
2020-04-23 18:28:13 -0700

    Better example

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,8 @@ import Browser
 import ElmFfi
 import Generated.ExampleApi as ExampleApi
 import Html exposing (..)
+import Html.Attributes as Attributes
+import Html.Events as Events
 import Json.Decode as Decode
 
 
@@ -12,7 +14,7 @@ main =
     Browser.element
         { init = init
         , view = view
-        , update = update
+        , update = \msg model -> ( update msg model, Cmd.none )
         , subscriptions = always Sub.none
         }
 
@@ -27,12 +29,15 @@ type alias Flags =
 
 type alias Model =
     { exampleApi : Result Decode.Error ExampleApi.Api
+    , locale : String
     }
 
 
 init : Flags -> ( Model, Cmd Msg )
 init { exampleApi } =
-    ( Model (Decode.decodeValue ExampleApi.decode exampleApi)
+    ( { exampleApi = Decode.decodeValue ExampleApi.decode exampleApi
+      , locale = "en"
+      }
     , Cmd.none
     )
 
@@ -45,15 +50,62 @@ view : Model -> Html Msg
 view model =
     main_
         []
-        [ case model.exampleApi of
-            Ok api ->
-                div []
-                    [ text <| Debug.toString (ExampleApi.randomNumber api)
-                    , text <| Debug.toString (ExampleApi.formatRelativeTime api 2 "day")
+        [ h1 [] [ text "Calling Foreign JavaScript Functions" ]
+        , section []
+            [ h2 [] [ text "Settings" ]
+            , label []
+                [ text "Locale"
+                , select
+                    [ Events.on "change"
+                        (Decode.at [ "target", "value" ] Decode.string
+                            |> Decode.map SelectLocale
+                        )
                     ]
+                    ([ ( "English", "en" )
+                     , ( "French", "fr" )
+                     , ( "Japanese", "ja" )
+                     ]
+                        |> List.map
+                            (\( label, value ) ->
+                                option [ Attributes.value value ] [ text label ]
+                            )
+                    )
+                ]
+            ]
+        , section []
+            [ h2 [] [ text "Output" ]
+            , case model.exampleApi of
+                Ok api ->
+                    table []
+                        [ thead []
+                            [ tr []
+                                [ th [] [ text "Function" ]
+                                , th [] [ text "Output" ]
+                                ]
+                            ]
+                        , tbody []
+                            [ tr []
+                                [ td [] [ text "Intl.NumberFormat" ]
+                                , td []
+                                    [ ExampleApi.formatCurrency api model.locale 4000
+                                        |> Result.withDefault ""
+                                        |> text
+                                    ]
+                                ]
+                            , tr []
+                                [ td [] [ text "Intl.formatRelativeTime" ]
+                                , td []
+                                    [ ExampleApi.formatRelativeTime api model.locale 5 "days"
+                                        |> Result.withDefault ""
+                                        |> text
+                                    ]
+                                ]
+                            ]
+                        ]
 
-            Err err ->
-                text <| "Failed to initialize program: " ++ Decode.errorToString err
+                Err err ->
+                    text <| "Failed to initialize foreign api: " ++ Decode.errorToString err
+            ]
         ]
 
 
@@ -62,11 +114,11 @@ view model =
 
 
 type Msg
-    = Noop
+    = SelectLocale String
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Noop ->
-            ( model, Cmd.none )
+        SelectLocale locale ->
+            { model | locale = locale }
